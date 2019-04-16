c9ea2dded83c43ddbc7c15e51fce2d1a406a67de
wujunt <wujunt@163.com>
2019-04-16 16:52:44 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,375 @@
+let resume = `# 吴俊涛
+186xxxxxxxxx | wujunt@163.com | 重庆
+18届本科毕业生 | 前端开发工程师
+
+## 专业技能
+- HTML5, CSS3 & JavaScript
+    - 能够使用原生 JavaScript 独立制作精美网页
+    - 掌握 HTML5 如 canvas 、 nav 等语义化标签
+    - 掌握 CSS3 动画、过渡、弹性盒子布局、媒体查询等响应式设计常用技术
+- Vue
+    - 了解其生命周期和基础操作
+- 语言能力
+    - 专业英语较好，词汇量大，阅读官方英语文档无压力
+- 其他
+    - 熟悉 jQuery 常用 API ，如 DOM 操作、特效、事件等。
+    - 熟练应用数据库MySQL，了解Oracle,SQL Server
+    - 理解 MVC 、 MVVM 思想，掌握 HTTP 基础等
+    - 掌握正则表达式基础
+    - 掌握Ajax基础
+
+
+
+
+## 项目经验
+### 简历生成器
+- 关键词：\`JavaScript\`、\`Promise\`、\`变速\`、\`过渡动画\`、\`盒阴影\`
+- 描述：该项目实现了一个 Markdown 简历的制作过程，在原生 JS 中使用 Promise 处理异步操作，利用 CSS3 实现过渡、动画、阴影和弹性布局等。
+- [源码链接]() | [预览链接]()
+
+
+## 教育经历
+### 福建福州闽江学院
+2013/9 - 2018/6
+- 专业学历：软件工程 大学本科
+- 荣誉：校二、三奖学金、优秀学生（2013-2016、2016-2018共两学年）、优秀学生干部（2013-2016、2016-2018共两学年）
+- 相关课程：网页设计、JavaWeb、数据结构与算法、数据库系统原理、计算机网络等
+`
+
+let code_ready = `/*
+ * 嗨，我是吴俊涛。
+ * 我想用代码给你展示一下我的个人简历～
+ * 灵感来自：http://strml.net
+ * 
+ * 那么，我要开始啦...
+ * 温馨提示：右上角小图标可以加速哦~
+ */
+
+/* 添加一个过渡，使变化看起来更柔和 */
+* {
+  transition: all 0.3s;
+}
+/* 加个背景色 */
+body {
+  background: #455A64;
+}
+/* 给代码加个边框 */
+#code_body {
+  width: 100%;
+  border: 1px solid #CFD8DC;
+  background: #CFD8DC;
+  padding: 16px;
+  overflow: auto;
+}
+
+/* 让代码高亮起来 */
+.token.comment, .token.punctuation {
+  color: #757575;
+}
+.token.selector {
+  color: #007400;
+}
+.token.property {
+  color: #cf1f1f;
+}
+
+/* 
+ * 给它们来个特效吧！
+ * 让它们在被编辑的时候产生“呼吸”的效果
+ */
+.breathe {
+  animation: breathe 3s ease 0s infinite;
+}
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
+}
+.breathe{
+  // position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+
+/* 现在开始写简历啦！ */
+
+/* 让代码框腾出一些的位置给我的简历框 */
+#options {
+  
+  width: 30%;
+}
+/* 准备一个白板 */
+#paper {
+  flex: 1;
+  background: #fdfdfd;
+  margin: 16px;
+  overflow: auto;
+  white-space: pre-wrap;
+}
+
+.paper_p{
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(-7deg) translateZ(-100px) ;
+          transform: rotateY(-7deg) translateZ(-100px) ;
+}
+/* 接下来，请看右边 */
+
+`
+
+let code_marked = `
+/* 
+ * 接下来我要使用一个优秀的库 marked.js
+ * 来使我的简历变成一篇 Markdown
+ * (https://github.com/markedjs/marked)
+ */
+`
+
+let code_beautify_resume = `
+/* 调整我的简历，让它变得再好看一点 */
+#paper {
+  white-space: unset;
+  padding: 0 32px;
+  font-size: 14px;
+}
+#paper a {
+  color: #455A64;
+}
+#paper ul {
+  padding-left: 20px;
+}
+/* 让每个模块上下间隔明显一点 */
+#paper > div {
+  margin: 28px 0;
+}
+
+/* 接下来是精细的调整 */
+
+/* 首先，调整我的“基础信息”板块 */
+#paper h1 {
+  font-size: 22px;
+  margin-bottom: 10px;
+}
+#information {
+  text-align: center;
+}
+#information p {
+  line-height: 24px;
+  white-space: pre-wrap;
+}
+
+/* 然后是详细介绍模块 */
+#paper h2 {
+  font-size: 16px;
+  border-bottom: 1px solid #455A64;
+  padding-bottom: 6px;
+  margin-bottom: 6px;
+}
+#paper h3 {
+  display: inline-block;
+  font-size: 14px;
+  margin: 6px 0;
+}
+#skills > ul ul {
+  margin: 4px 0;
+}
+#skills li, #works li, #education li {
+  margin-bottom: 4px;
+}
+#jobs > .experience {
+  margin-bottom: 6px;
+}
+
+/* 调整“教育经历”中时间的位置 */
+#jobs > .experience, #education {
+  position: relative;
+}
+#jobs p, #education p {
+  position: absolute;
+  right: 0px;
+}
+#jobs p {
+  top: 6px;
+}
+#education p {
+  top: 40px;
+}
+
+
+
+/* 
+ * 好啦
+ * 期待能加入贵公司~
+ */
+`
+
+var resumeInputTimeoutID, codeInputTimeoutID
+writeCode(code_ready, '').then(() => {
+  createResume().then(() => {
+    adjustResume().then(() => {
+      addAndAdjustAvatar().then(showDownloadButton)
+    }, () => {
+      showFinalResult()
+    })
+  })
+})
+
+var speedCode = 1, duration = 80
+$('#speed_btn').click(() => {
+  speedCode += 1
+  if (speedCode > 3) {
+    speedCode = 1
+  }
+  switch (speedCode) {
+    case 1:
+      speed_status.innerText = '🚲'
+      description.innerText = '开小车车快点吧~'
+      duration = 70
+      break
+    case 2:
+      speed_status.innerText = '🚗'
+      description.innerText = '太废话了，我要最快！'
+      duration = 30
+      break
+    case 3:
+      speed_status.innerText = '🚀'
+      description.innerText = '我眼花了！慢一点~'
+      duration = 0
+      break
+    default:
+      return 0
+  }
+})
+
+function writeCode(code, origin) {
+  let n = 1
+  return new Promise(resolve => {
+    codeInputTimeoutID = setTimeout(write, duration)
+    function write() {
+      code_body.innerHTML = Prism.highlight(origin + code.substr(0, n), Prism.languages.css, 'css');
+      code_style.innerHTML = origin + code.substr(0, n)
+      code_body.scrollTop = code_body.scrollHeight
+      n++
+      if (n === code.length) {
+        window.clearTimeout(codeInputTimeoutID)
+        resolve.call(undefined)
+      } else {
+        window.clearTimeout(codeInputTimeoutID)
+        codeInputTimeoutID = setTimeout(write, duration)
+      }
+    }
+  })
+}
+function writeResume() {
+  let n = 1
+  return new Promise(resolve => {
+    resumeInputTimeoutID = setTimeout(write, duration)
+    function write () {
+      paper.innerHTML = resume.substr(0, n)
+      paper.scrollTop = paper.scrollHeight
+      n++
+      if (n === resume.length) {
+        window.clearTimeout(resumeInputTimeoutID)
+        resolve.call(undefined)
+      } else {
+        window.clearTimeout(resumeInputTimeoutID)
+        resumeInputTimeoutID = setTimeout(write, duration)
+      }
+    }
+  })
+}
+
+function createResume() {
+  return new Promise(resolve => {
+    $('#paper').addClass('breathe')
+    $('#code_body').removeClass('breathe')
+    writeResume().then(resolve)
+    setTimeout(() => {
+      $('#skip_inputResume').show()
+      $('#skip_inputResume').click(() => {
+        skipResumeInput().then(resolve)
+      })
+    }, 1000)
+  })
+}
+function adjustResume() {
+  return new Promise((resolve, reject) => {
+    $('#code_body').addClass('breathe')
+    $('#paper').removeClass('breathe')
+    writeCode(code_marked, code_ready).then(() => {
+      structureResume()
+      $('#paper')[0].scrollTop = 0
+      writeCode(code_beautify_resume, code_ready + code_marked).then(resolve)
+      setTimeout(() => {
+        $('#skip_all').show()
+        $('#skip_all').click(() => {
+          skipAll().then(reject)
+        })
+      }, 1000)
+    })
+  })
+}
+function structureResume() {
+  $('#paper')[0].innerHTML = marked(resume)
+  $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="jobs"></div>', '<div id="works"></div>', '<div id="education"></div>')
+  $('#information').append($('h1'), $('p').first())
+  $('#skills').append($('h2:contains("技能")'), $('ul').first())
+  $('#education').append($('h2:contains("教育")'), $('h3:contains("学院")'), $('p').last(), $('ul').last())
+  $('#jobs').append($('h2:contains("工作")'))
+  $('h3:contains("公司")').each((index, element) => {
+    $('<div class="experience"></div>').append($(element), $('#paper > p').first(), $('#paper > ul').first()).appendTo($('#jobs'))
+  })
+  $('#works').append($('h2:contains("项目")'), $('#paper').children().not($('div')))
+  // $('a[href="./images/qrcode.png"]').addClass('qrcode-trigger').append($('img.qrcode'))
+}
+function addAndAdjustAvatar() {
+  return new Promise(resolve => {
+    $('#information').append($('img.avatar'))
+    writeCode(code_photo, code_ready + code_marked + code_beautify_resume).then(() => {
+      $('#paper').addClass('breathe')
+      $('#code_body').removeClass('breathe')
+      $('.skipper').remove()
+      setTimeout(resolve, 1000)
+    })
+  })
+}
+// function showDownloadButton() {
+//   $('#options').css({ 'width': '23%' })
+//   $('a.downloadResume').addClass('show')
+//   $('#options').append($('a.downloadResume'))
+// }
+
+function skipResumeInput() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      window.clearTimeout(resumeInputTimeoutID)
+      paper.innerHTML = resume
+      paper.scrollTop = paper.scrollHeight
+      resolve()
+      $('#skip_inputResume').remove()
+    }, 0)
+  })
+}
+function skipAll() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      window.clearTimeout(codeInputTimeoutID)
+      resolve()
+      $('.skipper').remove()
+    }, 0)
+  })
+}
+function showFinalResult() {
+  $('#paper').addClass('breathe')
+  $('#code_body').removeClass('breathe')
+  $('#information').append($('img.avatar'))
+  code_body.innerHTML = Prism.highlight(code_ready + code_marked + code_beautify_resume + code_photo, Prism.languages.css, 'css');
+  code_style.innerHTML = code_ready + code_marked + code_beautify_resume + code_photo
+  code_body.scrollTop = code_body.scrollHeight
+  window.clearTimeout(codeInputTimeoutID)
+  $('#options').css({ 'width': '23%' })
+  // $('a.downloadResume').addClass('show')
+ // $('#options').append($('a.downloadResume'))
+}
